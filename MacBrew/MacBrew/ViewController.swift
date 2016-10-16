cfdc9a41e2281b8b04d726f5d7c7f90598d6e5fc
Lars Nielsen <lnc13.lars@gmail.com>
2016-10-16 20:02:23 +0200

    update to ui

--- MacBrew/MacBrew/ViewController.swift
+++ MacBrew/MacBrew/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var packageList: NSTableView!
     
+    var packages:[Package]?
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        packageList.setDelegate(self)
+        packageList.setDataSource(self)
 
         // Do any additional setup after loading the view.
     }
@@ -39,5 +44,47 @@ class ViewController: NSViewController {
         packageList.endUpdates()
     }
 
+}
+
+extension ViewController : NSTableViewDataSource {
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return packages?.count ?? 0
+    }
+}
+
+extension ViewController : NSTableViewDelegate {
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+
+        var text:String = ""
+        var cellIdentifier: String = ""
+
+        // 1
+        guard let item = packages?[row] else {
+            return nil
+        }
+
+        // 2
+        if tableColumn == tableView.tableColumns[0] {
+            text = item._name
+            cellIdentifier = "nameCellId"
+        } else if tableColumn == tableView.tableColumns[1] {
+            text = item._description
+            cellIdentifier = "descriptionCellId"
+        } else if tableColumn == tableView.tableColumns[2] {
+            if !item._outdated {
+                text = "YES"
+            } else {
+                text = "NO"
+            }
+            cellIdentifier = "uptoDateCellId"
+        }
+
+        // 3
+        if let cell = tableView.makeViewWithIdentifier(cellIdentifier, owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            return cell
+        }
+        return nil
+    }
 }
 
